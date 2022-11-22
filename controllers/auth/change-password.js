772b5f6b6b209d3cb2605ea40b3e71cd141e8af4
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-11-22 12:38:30 +0700

    add JWT_SECRET_KEY at env

--- controllers/auth/change-password.js
+++ controllers/auth/change-password.js
@@ -1,1 +1,23 @@
 const { User } = require("../../models");
+const bcrypt = require("bcrypt");
+
+const changePassword = async (req, res, next) => {
+  const user = req.user;
+
+  const { oldPassword, newPassword, confirmNewPassword } = req.body;
+
+  if (newPassword != confirmNewPassword) {
+    return res.status(400).json({
+      status: false,
+      message: "Password Does't Match",
+      data: null,
+    });
+  }
+
+  const passwordUpdated = await User.update(
+    { password: newPassword },
+    { where: { id: user.id } }
+  );
+};
+
+module.exports = { changePassword };
