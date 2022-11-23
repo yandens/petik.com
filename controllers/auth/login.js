984c878bce78f2b13b637c81ecd8096c36585cb9
yandens <dedensetyawan17120104@gmail.com>
2022-11-23 17:59:34 +0800

    repair login and delete comment code

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -4,50 +4,52 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = process.env;
 
-
 const login = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-            const user = await User.findOne({ where: { email: email },
-            include: [{
-                model: Role,
-                as: 'role',
-            }] 
-        });
-            if (!user || user.status == false ) {
-                return res.status(400).json({
-                    status: false,
-                    message: 'email is not valid!',
-                    data: null
-                });
-            }
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: { email: email },
+      include: [{
+        model: Role,
+        as: 'role',
+      }]
+    });
+    const match = await bcrypt.compare(password, user.password);
 
-            const match = await bcrypt.compare(password, user.password);
-            if (!match) {
-                return res.status(400).json({
-                    status: false,
-                    message: 'password is not valid!',
-                    data: null
-                });
-            }
-
-            const payload = {
-                id: user.id,
-                email: user.email,
-                role: user.role.role
-            };
-            const token = jwt.sign(payload, JWT_SECRET_KEY);
-
-            return res.status(200).json({
-                status: true,
-                message: 'login success!',
-                data: { 
-                    email,
-                    token }
-            });
-    } catch (error) {
-        next(error);
+    if (!user || !match) {
+      return res.status(400).json({
+        status: false,
+        message: 'Wrong password or email!',
+        data: null
+      });
     }
+
+    if (user.status == false) {
+      return res.status(400).json({
+        status: false,
+        message: 'email is not valid!',
+        data: null
+      });
+    }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+      role: user.role.role
+    };
+    const token = jwt.sign(payload, JWT_SECRET_KEY);
+
+    return res.status(200).json({
+      status: true,
+      message: 'login success!',
+      data: {
+        email,
+        token
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports =  login ;
+module.exports = login;
