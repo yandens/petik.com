f3539396f583ec8e425b5bdff9f2d3d0546be5fd
yandens <dedensetyawan17120104@gmail.com>
2022-11-28 10:44:39 +0800

    repair db

--- db/config.js
+++ db/config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DB_PORT } =
+  process.env
 const { DB_USERNAME_DEV, DB_PASSWORD_DEV, DB_NAME_DEV, DB_HOST_DEV, DB_DIALECT_DEV, DB_PORT_DEV } =
   process.env;
 const { DB_USERNAME_PROD, DB_PASSWORD_PROD, DB_NAME_PROD, DB_HOST_PROD, DB_DIALECT_PROD, DB_PORT_PROD } =
@@ -7,12 +9,12 @@ const { DB_USERNAME_PROD, DB_PASSWORD_PROD, DB_NAME_PROD, DB_HOST_PROD, DB_DIALE
 
 module.exports = {
   development: {
-    username: DB_USERNAME_DEV,
-    password: DB_PASSWORD_DEV,
-    database: DB_NAME_DEV,
-    host: DB_HOST_DEV,
-    dialect: DB_DIALECT_DEV,
-    port: DB_PORT_DEV
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+    port: DB_PORT
   },
   test: {
     username: DB_USERNAME_DEV,
