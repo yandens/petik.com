56e8ad70c5e3e6878cce04135d4f04bf89563087
yandens <dedensetyawan17120104@gmail.com>
2022-11-28 11:28:38 +0800

    repair db & .env.example

--- db/config.js
+++ db/config.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DB_PORT } =
   process.env
-const { DB_USERNAME_DEV, DB_PASSWORD_DEV, DB_NAME_DEV, DB_HOST_DEV, DB_DIALECT_DEV, DB_PORT_DEV } =
-  process.env;
-const { DB_USERNAME_PROD, DB_PASSWORD_PROD, DB_NAME_PROD, DB_HOST_PROD, DB_DIALECT_PROD, DB_PORT_PROD } =
-  process.env
 
 module.exports = {
   development: {
