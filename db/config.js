c4df49927fce9b704302fbf29bae95c17c2dfb20
yandens <dedensetyawan17120104@gmail.com>
2022-11-28 09:36:54 +0800

    switch to remote database

--- db/config.js
+++ db/config.js
@@ -1,28 +1,33 @@
 require("dotenv").config();
 
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DB_PORT } =
+const { DB_USERNAME_DEV, DB_PASSWORD_DEV, DB_NAME_DEV, DB_HOST_DEV, DB_DIALECT_DEV, DB_PORT_DEV } =
   process.env;
+const { DB_USERNAME_PROD, DB_PASSWORD_PROD, DB_NAME_PROD, DB_HOST_PROD, DB_DIALECT_PROD, DB_PORT_PROD } =
+  process.env
 
 module.exports = {
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    username: DB_USERNAME_DEV,
+    password: DB_PASSWORD_DEV,
+    database: DB_NAME_DEV,
+    host: DB_HOST_DEV,
+    dialect: DB_DIALECT_DEV,
+    port: DB_PORT_DEV
   },
   test: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    username: DB_USERNAME_DEV,
+    password: DB_PASSWORD_DEV,
+    database: DB_NAME_DEV,
+    host: DB_HOST_DEV,
+    dialect: DB_DIALECT_DEV,
+    port: DB_PORT_DEV
   },
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    username: DB_USERNAME_PROD,
+    password: DB_PASSWORD_PROD,
+    database: DB_NAME_PROD,
+    host: DB_HOST_PROD,
+    dialect: DB_DIALECT_PROD,
+    port: DB_PORT_PROD
   },
 };
