d24ee8c8c18f7e4897718aa42c0fcaafee565c0c
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-11-28 09:53:07 +0700

    fix config

--- db/config.js
+++ db/config.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
 
-const { DB_USERNAME_DEV, DB_PASSWORD_DEV, DB_NAME_DEV, DB_HOST_DEV, DB_DIALECT_DEV, DB_PORT_DEV } =
-  process.env;
-const { DB_USERNAME_PROD, DB_PASSWORD_PROD, DB_NAME_PROD, DB_HOST_PROD, DB_DIALECT_PROD, DB_PORT_PROD } =
-  process.env
+const {
+  DB_USERNAME_DEV,
+  DB_PASSWORD_DEV,
+  DB_NAME_DEV,
+  DB_HOST_DEV,
+  DB_DIALECT_DEV,
+  DB_PORT_DEV,
+} = process.env;
+const {
+  DB_USERNAME_PROD,
+  DB_PASSWORD_PROD,
+  DB_NAME_PROD,
+  DB_HOST_PROD,
+  DB_DIALECT_PROD,
+  DB_PORT_PROD,
+} = process.env;
 
 module.exports = {
   development: {
@@ -12,7 +24,7 @@ module.exports = {
     database: DB_NAME_DEV,
     host: DB_HOST_DEV,
     dialect: DB_DIALECT_DEV,
-    port: DB_PORT_DEV
+    port: DB_PORT_DEV,
   },
   test: {
     username: DB_USERNAME_DEV,
@@ -20,7 +32,7 @@ module.exports = {
     database: DB_NAME_DEV,
     host: DB_HOST_DEV,
     dialect: DB_DIALECT_DEV,
-    port: DB_PORT_DEV
+    port: DB_PORT_DEV,
   },
   production: {
     username: DB_USERNAME_PROD,
@@ -28,6 +40,6 @@ module.exports = {
     database: DB_NAME_PROD,
     host: DB_HOST_PROD,
     dialect: DB_DIALECT_PROD,
-    port: DB_PORT_PROD
+    port: DB_PORT_PROD,
   },
 };
