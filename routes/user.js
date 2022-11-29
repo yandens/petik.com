ce1f4899efb799d84e0b17cc7d875d9927c469a9
yandens <dedensetyawan17120104@gmail.com>
2022-11-29 21:39:28 +0800

    add middleware authorize

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const authorize = require("../middlewares/authorize");
 const user = require("../controllers/user");
+const role = require("../utils/role-based/role")
 
-router.put("delete-account", user.deleteAccount);
-router.get("get-users", user.getAllUser);
+router.put("/delete-account", authorize(role.buyer), user.deleteAccount);
+router.get("/get-users", authorize(role.admin), user.getAllUser);
 
 module.exports = router;
