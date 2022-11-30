a5fc94aac9c77741362f10f7b6304f087d69536d
yandens <dedensetyawan17120104@gmail.com>
2022-11-30 21:36:11 +0800

    add try catch

--- controllers/user/getUser.js
+++ controllers/user/getUser.js
@@ -1,21 +1,25 @@
 const { User, UserBiodata } = require('../../models')
 
 const getUser = async (req, res, next) => {
-  const { user_id } = req.params
+  try {
+    const { user_id } = req.params
 
-  const user = await User.findOne({
-    where: { id: user_id },
-    include: [{
-      model: UserBiodata,
-      as: "biodata"
-    }]
-  })
+    const user = await User.findOne({
+      where: { id: user_id },
+      include: [{
+        model: UserBiodata,
+        as: "biodata"
+      }]
+    })
 
-  return res.status(200).json({
-    status: true,
-    message: 'Success get user data!',
-    data: user
-  })
+    return res.status(200).json({
+      status: true,
+      message: 'Success get user data!',
+      data: user
+    })
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = getUser
