d6c902c2d15a0832e473f84e860f2da40dbdf7ce
yandens <dedensetyawan17120104@gmail.com>
2022-11-30 21:35:57 +0800

    repair get user endpoint

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,6 @@ router.post("/create-bio", authorize(role.buyer), user.createBio);
 router.put("/update-bio", authorize(role.buyer), user.updateBio);
 router.put("/delete-account", authorize(role.buyer), user.deleteAccount);
 router.get("/get-users", authorize(role.admin), user.getAllUser);
-router.get("/get-user", authorize(role.admin), user.getUser);
+router.get("/get-user/:user_id", authorize(role.admin), user.getUser);
 
 module.exports = router;
