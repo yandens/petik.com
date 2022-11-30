c4bc6c44b02f1a40ff853ecdeb873911c441a899
yandens <dedensetyawan17120104@gmail.com>
2022-11-30 12:32:20 +0800

    repair middleware authorize

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -8,7 +8,7 @@ function authorize(roles = []) {
   return [
     (req, res, next) => {
       try {
-        const token = req.headers["authorization"];
+        const token = req.headers["authorization"].split(" ")[1];
 
         if (!token) {
           return res.status(401).json({
