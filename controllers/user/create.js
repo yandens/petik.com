571a4e653398fb86fd2599bb5621757dccdd58f9
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-02 21:07:15 +0700

    add phoneNumber json

--- controllers/user/create.js
+++ controllers/user/create.js
@@ -1,34 +1,30 @@
-const { User, UserBiodata } = require('../../models');
-const Validator = require('fastest-validator');
+const { User, UserBiodata } = require("../../models");
+const Validator = require("fastest-validator");
 const v = new Validator({
   useNewCustomCheckerFunction: true, // using new version
   messages: {
     // Register our new error message text
-    phoneNumber: "The phone number must be started with '+'!"
-  }
+    phoneNumber: "The phone number must be started with '+'!",
+  },
 });
 
 const createBio = async (req, res, next) => {
   try {
-    const user = req.user
-    const {
-      firstName,
-      lastName,
-      gender,
-      phoneNumber,
-      address,
-      nationality
-    } = req.body;
+    const user = req.user;
+    const { firstName, lastName, gender, phoneNumber, address, nationality } =
+      req.body;
 
     const schema = {
       firstName: { type: "string" },
       lastName: { type: "string" },
       gender: { type: "string" },
       phoneNumber: {
-        type: "string", min: 12, custom: (v, err) => {
-          if (!v.startsWith("+")) err.push({ type: "phoneNumber" })
+        type: "string",
+        min: 12,
+        custom: (v, err) => {
+          if (!v.startsWith("+")) err.push({ type: "phoneNumber" });
           return v.replace(/[^\d+]/g, ""); // Sanitize: remove all special chars except numbers
-        }
+        },
       },
       address: { type: "string" },
       nationality: { type: "string" },
@@ -53,20 +49,26 @@ const createBio = async (req, res, next) => {
     }
 
     await UserBiodata.create({
-      user_id: user.id, firstName, lastName, gender, phoneNumber, address, nationality
+      user_id: user.id,
+      firstName,
+      lastName,
+      gender,
+      phoneNumber,
+      address,
+      nationality,
     });
 
     return res.status(201).json({
       status: true,
-      message: 'success',
+      message: "success",
       data: {
         id: user.id,
-        email: user.email
-      }
+        email: user.email,
+      },
     });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
-module.exports = createBio
+module.exports = createBio;
