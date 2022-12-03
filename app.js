a7e74dbf19c04b0f88c44fa85fbafe4b44602014
yandens <dedensetyawan17120104@gmail.com>
2022-12-03 22:07:28 +0800

    add sentry for error logs

--- app.js
+++ app.js
@@ -8,8 +8,24 @@ const ejs = require("ejs");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
 
 const app = express();
+
+Sentry.init({
+  dsn: "https://f2f5e64d065541e5ac86af0eccc6f194@o4504076201164800.ingest.sentry.io/4504265416900608",
+  environment: process.env.NODE_ENV,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.set("view engine", "ejs");
 app.use(morgan("dev")); // for logging
@@ -34,6 +50,12 @@ app.use((err, req, res, next) => {
     status: false,
     message: err.message,
   });
+});
+
+app.use(Sentry.Handlers.errorHandler());
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
 
 const { PORT } = process.env;
