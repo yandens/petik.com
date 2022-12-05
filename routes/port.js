86d4b0169f60c067d4626235ce7d18f9a907b795
yandens <dedensetyawan17120104@gmail.com>
2022-12-05 21:14:28 +0800

    repair swagger docs and add middleware authorize

--- routes/port.js
+++ routes/port.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const port = require("../controllers/port");
+const authorize = require("../middlewares/authorize");
 
-router.get("/search/:search", port.getPort);
+router.get("/search/:search", authorize(), port.getPort);
 
 module.exports = router;
