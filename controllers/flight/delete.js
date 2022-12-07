885ea2e1ba5f60bf497189931e3a1d979ec43b48
Wayan Deden Setyawan <81473098+yandens@users.noreply.github.com>
2022-12-07 09:28:33 +0800

    Update delete.js

--- controllers/flight/delete.js
+++ controllers/flight/delete.js
@@ -3,23 +3,16 @@ const { Flight } = require('../../models');
 const deleteFlight = async  (req, res, next) => {
     try {
         const { id } = req.body;
-        const deleted = await Flight.destroy(
-            { id_airlines,
-              departure,
-              arrival,
-              departureTime,
-              arrivalTime
-            },
-            { where: { id } }
-        );
+        const deleted = await Flight.destroy({ where: { id } });
 
         return res.status(200).json({
             status: true,
-            message: "Flight Deleted success",
+            message: "Flight Deleted!",
             data: deleted,
         });
     } catch (err) {
         next(err);
     }
 }
+
 module.exports = deleteFlight;
