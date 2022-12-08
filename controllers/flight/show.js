cd2b5789cf0f26499238903026f46e3113818f4d
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-08 13:19:24 +0700

    fix show endpoint

--- controllers/flight/show.js
+++ controllers/flight/show.js
@@ -1,25 +1,16 @@
-const { Flight } = require('../../models')
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const { GOFLIGHTLABS_ACCESS_KEY } = process.env;
-
-const searchAirport = async (iataCode) => {
-  const url = `https://port-api.com/airport/iata/${iataCode}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Host": "port-api.com",
-    },
-  };
-  const result = await fetch(url, options);
-  const json = await result.json();
-  const airport = json.properties.name;
-  return airport;
-};
+const { Flight } = require("../../models");
 
 const showFlight = async (req, res, next) => {
   try {
-    const allFlight = await Flight.findAll()
+    const allFlight = await Flight.findAll();
+
+    if (allFlight <= 0) {
+      return res.status(400).json({
+        status: false,
+        message: "No Flight",
+      });
+    }
+
     return res.status(200).json({
       status: true,
       message: "Success Get All Data",
