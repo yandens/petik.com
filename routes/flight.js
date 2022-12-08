39503895b316fec203c7b5072b4a674a88d16014
yandens <dedensetyawan17120104@gmail.com>
2022-12-08 20:55:34 +0800

    change to post method and add doc for flight

--- routes/flight.js
+++ routes/flight.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const flight = require("../controllers/flight");
 
 router.get("/schedule", flight.showFlight);
-router.get("/schedule/search", flight.showFilter);
+router.post("/schedule/search", flight.showFilter);
 
 module.exports = router;
