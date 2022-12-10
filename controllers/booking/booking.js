cc48572c799baf7ec2a16aaa4c94f703ffaacbb0
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-10 12:40:13 +0700

    add ticket endpoint

--- controllers/booking/booking.js
+++ controllers/booking/booking.js
@@ -8,7 +8,7 @@ const createBooking = async (req, res, next) => {
 
     const booking = await Booking.create({
       user_id: user.id,
-      status: 'pending',
+      status: "pending",
       date: new Date(),
     });
 
@@ -17,29 +17,33 @@ const createBooking = async (req, res, next) => {
       flight_id,
       passangerName,
       NIK,
-      passport
-    })
+      passport,
+    });
 
-    let totalPrice, different
-    if (ticketClass == 'economy') {
-      different = 321.37 - 64.27
-      totalPrice = Math.floor((Math.random() * different) + 64.27);
-    } else if (ticketClass == 'business') {
-      different = 642.74 - 321.37
-      totalPrice = Math.floor((Math.random() * different) + 321.37);
+    let totalPrice, different;
+    if (ticketClass == "economy") {
+      different = 321.37 - 64.27;
+      totalPrice = Math.floor(Math.random() * different + 64.27);
+    } else if (ticketClass == "business") {
+      different = 642.74 - 321.37;
+      totalPrice = Math.floor(Math.random() * different + 321.37);
     } else {
-      different = 1606.84 - 642.74
-      totalPrice = Math.floor((Math.random() * different) + 642.74);
+      different = 1606.84 - 642.74;
+      totalPrice = Math.floor(Math.random() * different + 642.74);
     }
 
     return res.status(201).json({
       status: true,
-      message: 'Booking Created!',
-      data: { booking, bookingDetails, totalPrice }
-    })
+      message: "Booking Created!",
+      data: {
+        booking: booking,
+        booking_details: bookingDetails,
+        total_price: totalPrice,
+      },
+    });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = createBooking
+module.exports = createBooking;
--- controllers/ticket/ticket.js
+++ controllers/ticket/ticket.js
@@ -1,11 +1,78 @@
-const { Booking, BookingDetails, Flight, Ticket } = require("../../models")
-const { Op } = require('sequelize')
+const { Booking, BookingDetails, Flight, Ticket } = require("../../models");
+const generateQRCode = require("../../utils/media/generateQrCode");
+const imagekit = require("../../utils/media/imageKit");
+// const { Op } = require("sequelize");
+
+const uploadQRCode = async (file) => {
+  try {
+    const uploadedFile = await imagekit.upload({
+      file,
+      fileName: `qrCode`,
+    });
+    const data = {
+      title: uploadedFile.name,
+      url: uploadedFile.url,
+    };
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const printTicket = async (req, res, next) => {
   try {
-    const user = req.user
+    const user = req.user;
+    const { id } = req.params;
 
+    const getTickets = await Ticket.findAll(
+      {
+        where: { booking_details_id: id },
+      },
+      {
+        include: [
+          {
+            model: BookingDetails,
+            as: "details",
+            include: [
+              {
+                model: Booking,
+                as: "booking",
+              },
+              {
+                model: Flight,
+                as: "flight",
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+    const qrCode = generateQRCode(JSON.stringify(payload));
+    const uploadQR = await uploadQRCode(qrCode.toString("base64"));
+
+    return res.status(200).json({
+      status: true,
+      message: "Success",
+      data: {
+        id: getTickets.details.booking.user_id,
+        name: getTickets.details.passangerName,
+        airline: getTickets.details.flight.airline,
+        from: getTickets.details.flight.origin,
+        to: getTickets.details.flight.destination,
+        date: getTickets.details.flight.departure.toDateString(),
+        boarding_time: getTickets.details.flight.departure.toLocaleTimeString(),
+        class: getTickets.class,
+        qr_code: uploadQR.url,
+      },
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
+
+module.exports = printTicket;
