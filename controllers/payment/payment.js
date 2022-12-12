abd44c94d945e728dfdfb0a4043d859445b8c8f0
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-12 22:18:14 +0700

    fix response

--- controllers/payment/payment.js
+++ controllers/payment/payment.js
@@ -1,58 +1,66 @@
-const { Booking, BookingDetails, Flight, Payment, PaymentMethod, Ticket } = require("../../models")
+const {
+  Booking,
+  BookingDetails,
+  Flight,
+  Payment,
+  PaymentMethod,
+  Ticket,
+} = require("../../models");
 const { Op } = require("sequelize");
 
 const payment = async (req, res, next) => {
   try {
-    const user = req.user
-    const { paymentMethod, totalPrice } = req.body
-    const { booking_id, ticketClass } = req.params
+    const user = req.user;
+    const { paymentMethod, totalPrice } = req.body;
+    const { booking_id, ticketClass } = req.params;
 
-    /*const booking = await Booking.findOne({
-      where: {
-        [Op.and] : [{user_id: user.id}, {status: 'pending'}]
-      }
-    })
+    const book = await Booking.findOne({ where: { id: booking_id } });
 
-    if(booking.length <= 0){
+    if (book.status != "pending") {
       return res.status(400).json({
         status: false,
-        message: "You're not booking yet",
-        data: null
-      })
-    }*/
+        message: "You can't pay paid booking or canceled booking!",
+        data: null,
+      });
+    }
 
-    const method = await PaymentMethod.findOne({ where: { method: paymentMethod } })
+    const payMethod = await PaymentMethod.findOne({
+      where: { method: paymentMethod },
+    });
+
     const payment = await Payment.create({
       booking_id,
-      payement_method_id: method.id,
+      payment_method_id: payMethod.id,
       total_price: totalPrice,
-      date: new Date()
-    })
+      date: new Date(),
+    });
 
-    await Booking.update({ status: 'paid' }, { where: { id: booking_id } })
+    await Booking.update({ status: "paid" }, { where: { id: booking_id } });
     const booking = await Booking.findOne({
       where: {
-        [Op.and]: [{ user_id: user.id }, { status: 'paid' }]
-      }
-    })
-    const bookingDetails = await BookingDetails.findAll({ where: { booking_id: booking.id } })
+        [Op.and]: [{ user_id: user.id }, { status: "paid" }],
+      },
+    });
+    const bookingDetails = await BookingDetails.findAll({
+      where: { booking_id: booking.id },
+    });
 
     for (const details of bookingDetails) {
       await Ticket.create({
         booking_details_id: details.id,
         seatNumber: null,
-        class: ticketClass
-      })
+        class: ticketClass,
+      });
     }
 
     return res.status(201).json({
       status: true,
-      message: 'Successful Payment',
-      data: payment
-    })
+      message: "Successful Payment",
+      data: payment,
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
-module.exports = payment
+module.exports = payment;
