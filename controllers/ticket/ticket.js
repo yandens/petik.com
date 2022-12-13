b462e75f19cf55f88d4bcc86bac3e60ce01d3a18
yandens <dedensetyawan17120104@gmail.com>
2022-12-13 21:06:19 +0800

    repair ticket controller

--- controllers/ticket/ticket.js
+++ controllers/ticket/ticket.js
@@ -48,40 +48,45 @@ const printTicket = async (req, res, next) => {
       ],
     });
 
-    const departureTime = getTickets.details[0].flight.departure;
+    const data = []
+    for (const ticket of getTickets.details) {
+      const departureTime = ticket.flight.departure
+      const payload = {
+        id: getTickets.user_id,
+        booking_id: getTickets.id,
+        name: ticket.passangerName,
+        airline: ticket.flight.airline,
+        from: ticket.flight.origin,
+        to: ticket.flight.destination,
+        date: ticket.flight.departure.toDateString(),
+        departureTime: ticket.flight.departure.toLocaleTimeString(),
+        dep: departureTime,
+        class: ticket.ticket.class,
+      }
 
-    const payload = {
-      id: getTickets.user_id,
-      booking_id: getTickets.id,
-      name: getTickets.details[0].passangerName,
-      airline: getTickets.details[0].flight.airline,
-      from: getTickets.details[0].flight.origin,
-      to: getTickets.details[0].flight.destination,
-      date: getTickets.details[0].flight.departure.toDateString(),
-      departureTime:
-        getTickets.details[0].flight.departure.toLocaleTimeString(),
-      dep: departureTime,
-      class: getTickets.details[0].ticket.class,
-    };
+      const qrCode = generateQRCode(JSON.stringify(payload));
+      const uploadQR = await uploadQRCode(qrCode.toString("base64"));
 
-    const qrCode = generateQRCode(JSON.stringify(payload));
-    const uploadQR = await uploadQRCode(qrCode.toString("base64"));
+      const dataResponse = {
+        id: getTickets.user_id,
+        booking_id: getTickets.id,
+        name: ticket.passangerName,
+        airline: ticket.flight.airline,
+        from: ticket.flight.origin,
+        to: ticket.flight.destination,
+        date: ticket.flight.departure.toDateString(),
+        departureTime: departureTime.toLocaleTimeString(),
+        boardingTime: subtractHours(departureTime, 1).toLocaleTimeString(),
+        qr_code: uploadQR.url,
+      }
+
+      data.push(dataResponse)
+    }
 
     return res.status(200).json({
       status: true,
       message: "Success",
-      data: {
-        id: getTickets.user_id,
-        booking_id: getTickets.id,
-        name: getTickets.details[0].passangerName,
-        airline: getTickets.details[0].flight.airline,
-        from: getTickets.details[0].flight.origin,
-        to: getTickets.details[0].flight.destination,
-        date: getTickets.details[0].flight.departure.toDateString(),
-        departureTime: departureTime.toLocaleTimeString(),
-        boardingTime: subtractHours(departureTime, 1).toLocaleTimeString(),
-        qr_code: uploadQR.url,
-      },
+      data
     });
   } catch (err) {
     next(err);
