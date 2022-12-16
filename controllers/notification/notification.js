fdc3587df01b298ad915f962d73aa2a7f0dfb52d
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-16 13:32:21 +0700

    create endpoint notifications

---
+++ controllers/notification/notification.js
@@ -0,0 +1,16 @@
+// const { Notification } = require("../../models");
+
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// const notifications = (req, res, next) => {
+//   try {
+//     const { user_id } = req.params;
+//   } catch (error) {
+//     next(error);
+//   }
+// };
