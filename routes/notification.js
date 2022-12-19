99aba14d251e4f927faf5a0d56e15f4b0e45337d
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-19 08:28:44 +0700

    fix routes

--- routes/notification.js
+++ routes/notification.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const getNotification = require("../controllers/notification");
 const authorize = require("../middlewares/authorize");
 
-router.get("/:id", authorize(), getNotification.getNotification);
+router.get("/", authorize(), getNotification.getNotification);
 
 module.exports = router;
