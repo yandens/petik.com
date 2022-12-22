772011e586ea6bc0f980f1a7f351347a42f8e380
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-22 10:24:24 +0700

    fix response getUser

--- controllers/user/getUser.js
+++ controllers/user/getUser.js
@@ -1,33 +1,48 @@
-const { User, UserBiodata } = require('../../models')
+const { User, UserBiodata } = require("../../models");
 
 const getUser = async (req, res, next) => {
   try {
-    const { user_id } = req.params
+    const { user_id } = req.params;
 
     const user = await User.findOne({
       where: { id: user_id },
-      include: [{
-        model: UserBiodata,
-        as: "biodata"
-      }]
-    })
+      include: [
+        {
+          model: UserBiodata,
+          as: "biodata",
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(400).json({
         status: false,
-        message: 'User not found!',
-        data: null
-      })
+        message: "User not found!",
+        data: null,
+      });
     }
 
     return res.status(200).json({
       status: true,
-      message: 'Success get user data!',
-      data: user
-    })
+      message: "Success get user data!",
+      data: {
+        id: user.id,
+        email: user.email,
+        roleId: user.role_id,
+        userType: user.user_type,
+        status: user.status,
+        isActive: user.isActive,
+        firstName: user.biodata.firstName,
+        lastName: user.biodata.lastName,
+        gender: user.biodata.gender,
+        phoneNumber: user.biodata.phoneNumber,
+        address: user.biodata.address,
+        nationality: user.biodata.nationality,
+      },
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
-module.exports = getUser
+module.exports = getUser;
