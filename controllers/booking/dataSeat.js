38a6ca084133842e8a48d69a11baed8fb9da409a
yandens <dedensetyawan17120104@gmail.com>
2022-12-23 22:35:35 +0800

    add total user booking and list seat reserved controller

---
+++ controllers/booking/dataSeat.js
@@ -0,0 +1,36 @@
+const { BookingDetails, Ticket } = require('../../models')
+
+const dataSeat = async (req, res, next) => {
+  try {
+    const { flight_id } = req.params
+
+    const seatData = await BookingDetails.findAll({
+      where: { flight_id },
+      include: [{
+        model: Ticket,
+        as: 'ticket'
+      }]
+    })
+
+    const listSeat = []
+    for (const seat of seatData) listSeat.push(seat.ticket.seatNumber)
+
+    if (listSeat.length <= 0) {
+      return res.status(204).json({
+        status: false,
+        message: 'No reserved seats!',
+        data: null
+      })
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: 'Succes get the reserved seat!',
+      data: listSeat
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = dataSeat
