f02ec112e1ca0a2966513068a2c4fe18dfc9313f
yandens <dedensetyawan17120104@gmail.com>
2022-12-25 13:46:01 +0800

    add get flight endpoint and repair get user endpoint

--- routes/flight.js
+++ routes/flight.js
@@ -5,18 +5,9 @@ const role = require("../utils/role-based/role");
 
 router.get("/schedule", flight.showFlight);
 router.post("/schedule/search", flight.showFilter);
-router.post(
-  "/create-flight",
-  authorize(role.admin),
-  flight.createFlight.createFlightAdmin
-);
-
+router.post("/create-flight", authorize(role.admin), flight.createFlight.createFlightAdmin);
 router.put("/edit/:flight_id", authorize(role.admin), flight.updateFlight);
-
-router.delete(
-  "/edit/:flight_id",
-  authorize(role.admin),
-  flight.deleteFlight.deleteFlightAdmin
-);
+router.delete("/edit/:flight_id", authorize(role.admin), flight.deleteFlight.deleteFlightAdmin);
+router.get("/get-flight/:flight_id", authorize(role.admin), flight.getFlight)
 
 module.exports = router;
