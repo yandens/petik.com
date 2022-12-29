a410336e3b6a1cb2fa7ce8f1070eb1d9a166f978
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-12-29 20:25:09 +0700

    undo change response

--- controllers/booking/getAllBooking.js
+++ controllers/booking/getAllBooking.js
@@ -1,15 +1,19 @@
 const { Booking } = require("../../models");
 
 const getAllBooking = async (req, res, next) => {
-  const user = req.user;
+  try {
+    const user = req.user;
 
-  const bookings = await Booking.findAll({ where: { user_id: user.id } });
+    const bookings = await Booking.findAll({ where: { user_id: user.id } });
 
-  return res.status(200).json({
-    status: true,
-    message: "Success Get Data",
-    data: bookings,
-  });
+    return res.status(200).json({
+      status: true,
+      message: "Success Get Data",
+      data: bookings,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getAllBooking;
